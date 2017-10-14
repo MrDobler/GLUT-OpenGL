59f108183bfea6d59400b0c73300d96b4c10cc50
MrDobler <arthur.dobler@gmail.com>
2017-10-13 22:06:56 -0300

    Melhorias na animação, criação do resto do carro e criação das faixas da rua

--- main.cpp
+++ main.cpp
@@ -24,20 +24,20 @@ void desenha(void)
 
     //Cria a casa
 
-		glBegin(GL_QUADS);
-			glColor3f( 1, 1, 1);
-			glVertex2f(-18, 0);
-			glVertex2f(-18, 4);
-			glVertex2f(-12, 4);
-			glVertex2f(-12, 0);
-		glEnd();
+	glBegin(GL_QUADS);
+		glColor3f( 1, 1, 1);
+		glVertex2f(-18, 0);
+		glVertex2f(-18, 4);
+		glVertex2f(-12, 4);
+		glVertex2f(-12, 0);
+	glEnd();
 
-		glBegin(GL_TRIANGLES);
-			glColor3f( 0.255, 0, 0);
-			glVertex2f(-18, 4);
-			glVertex2f(-12, 4);
-			glVertex2f(-15 ,6);
-		glEnd();
+	glBegin(GL_TRIANGLES);
+		glColor3f( 0.255, 0, 0);
+		glVertex2f(-18, 4);
+		glVertex2f(-12, 4);
+		glVertex2f(-15 ,6);
+	glEnd();
 
 
     //Cria a rua
@@ -47,6 +47,24 @@ void desenha(void)
 		glVertex2f(-2, 20);
 		glVertex2f( 2, 20);
 		glVertex2f( 8, 0);
+	glEnd();
+
+	//Cria faixa esquerda da rua
+	glBegin(GL_QUADS);
+		glColor3f( 1, 1, 1);
+		glVertex2f(-8.5, 0);
+		glVertex2f(-2.5, 20);
+		glVertex2f( -2, 20);
+		glVertex2f( -8, 0);
+	glEnd();
+
+	//Cria faixa a direita da rua
+	glBegin(GL_QUADS);
+		glColor3f( 1, 1, 1);
+		glVertex2f( 8.5, 0);
+		glVertex2f( 2.5, 20);
+		glVertex2f(  2, 20);
+		glVertex2f(  8, 0);
 	glEnd();
 
     //Cria a árvore
@@ -70,50 +88,81 @@ void desenha(void)
 		glVertex2f( 16, 0);
 	glEnd();
 
-    //Cria a lataria do carro
-	glBegin(GL_QUADS);
-		glColor3f(0.137255, 0.137255, 0.556863);
-		glVertex2f(-3, 2);
-		glVertex2f(-3, 4);
-		glVertex2f( 3, 4);
-		glVertex2f( 3, 2);
-	glEnd();
+	glPushMatrix();
+		glScalef(escala, escala, 0.0f);
+		//Cria a lataria do carro
+		glBegin(GL_QUADS);
+			glColor3f(0.137255, 0.137255, 0.556863);
+			glVertex2f(-3, 2);
+			glVertex2f(-3, 4);
+			glVertex2f( 3, 4);
+			glVertex2f( 3, 2);
+		glEnd();
 
-	//Cria as rodas do carro
-	glBegin(GL_QUADS);
-		glColor3f( 0, 0, 0);
-		glVertex2f(-2.5, 1.5);
-		glVertex2f(-2.5, 2);
-		glVertex2f(-1.5, 2);
-		glVertex2f(-1.5, 1.5);
-	glEnd();
+		//Cria o teto do carro
+		glBegin(GL_QUADS);
+			glColor3f(0.137255, 0.137255, 0.556863);
+			glVertex2f(-2.5, 4);
+			glVertex2f(-1.5, 5);
+			glVertex2f( 1.5, 5);
+			glVertex2f( 2.5, 4);
+		glEnd();
 
-	glBegin(GL_QUADS);
-		glColor3f( 0, 0, 0);
-		glVertex2f( 1.5, 1.5);
-		glVertex2f( 1.5, 2);
-		glVertex2f( 2.5, 2);
-		glVertex2f( 2.5, 1.5);
-	glEnd();
+		//Cria o vidro traseiro
+		glBegin(GL_QUADS);
+			glColor3f(0.560784, 0.560784, 0.737255);
+			glVertex2f( -2, 4);
+			glVertex2f(-1.5, 4.7);
+			glVertex2f( 1.5, 4.7);
+			glVertex2f( 2, 4);
+		glEnd();
 
 
-	//Cria lanternas do carro
-	glBegin(GL_QUADS);
-		glColor3f( 1, 0, 0);
-		glVertex2f( -2.5, 3);
-		glVertex2f( -2.5, 3.5);
-		glVertex2f( -1, 3.5);
-		glVertex2f( -1, 3);
-	glEnd();
+		//Cria as rodas do carro
+		glBegin(GL_QUADS);
+			glColor3f( 0, 0, 0);
+			glVertex2f(-2.5, 1.5);
+			glVertex2f(-2.5, 2);
+			glVertex2f(-1.5, 2);
+			glVertex2f(-1.5, 1.5);
+		glEnd();
 
-	glBegin(GL_QUADS);
-		glColor3f( 1, 0, 0);
-		glVertex2f( 1, 3);
-		glVertex2f( 1, 3.5);
-		glVertex2f( 2.5, 3.5);
-		glVertex2f( 2.5, 3);
-	glEnd();
+		glBegin(GL_QUADS);
+			glColor3f( 0, 0, 0);
+			glVertex2f( 1.5, 1.5);
+			glVertex2f( 1.5, 2);
+			glVertex2f( 2.5, 2);
+			glVertex2f( 2.5, 1.5);
+		glEnd();
 
+
+		//Cria lanternas do carro
+		glBegin(GL_QUADS);
+			glColor3f( 1, 0, 0);
+			glVertex2f( -2.5, 3);
+			glVertex2f( -2.5, 3.5);
+			glVertex2f( -1, 3.5);
+			glVertex2f( -1, 3);
+		glEnd();
+
+		glBegin(GL_QUADS);
+			glColor3f( 1, 0, 0);
+			glVertex2f( 1, 3);
+			glVertex2f( 1, 3.5);
+			glVertex2f( 2.5, 3.5);
+			glVertex2f( 2.5, 3);
+		glEnd();
+
+		//Cria a placa do carro
+		glBegin(GL_QUADS);
+			glColor3f( 0.658824, 0.658824, 0.658824);
+			glVertex2f( -0.5, 2.5);
+			glVertex2f( -0.5, 2.8);
+			glVertex2f( 0.5, 2.8);
+			glVertex2f( 0.5, 2.5);
+		glEnd();
+
+	glPopMatrix();
 
 	glutSwapBuffers();
 
@@ -126,10 +175,10 @@ void teclado(unsigned char tecla, GLint x, GLint y)
     switch(tecla)
     {
         case '+':
-            escala++;
+            escala = escala + 0.1;
             break;
         case '-':
-            escala--;
+            escala = escala - 0.1;
             break;
     }
     desenha();
